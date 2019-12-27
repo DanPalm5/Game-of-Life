7a57888efdcdb22c9a6ca3461a2a3fdb2a125041
Earl Kennedy <mnenmenth@gmail.com>
2019-12-26 23:34:01 -0500

    Template for entity sibling check functions

--- src/gameoflife/game/Entity.kt
+++ src/gameoflife/game/Entity.kt
@@ -1,8 +1,17 @@
 package gameoflife.game
 
-enum class EntityState(val state: Int)
+enum class EntityState(private val state: Int)
 {
-    DEAD(0), ALIVE(1)
+    DEAD(0)
+        {
+            override fun toString(): String = "DEAD"
+        },
+    ALIVE(1)
+        {
+            override fun toString(): String = "ALIVE"
+        };
+
+    abstract override fun toString(): String
 }
 
 class Entity(val initState: EntityState = EntityState.DEAD, val position: Pair<Int, Int>)
@@ -10,9 +19,6 @@ class Entity(val initState: EntityState = EntityState.DEAD, val position: Pair<I
 
 
 
-    fun topRightSib(): Int?
-    {
-        return null
-    }
+    val topRightSib: Pair<Int, Int> = Pair(position.first-1, position.second+1)
 
 }
