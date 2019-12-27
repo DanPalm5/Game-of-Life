84e6c30aefb361042d2dff8d6205e987f60ed701
Daniel Palmieri <57024625+DanPalm5@users.noreply.github.com>
2019-12-26 22:58:08 -0500

    initialized gameboard

--- src/gameoflife/main/GameOfLife.kt
+++ src/gameoflife/main/GameOfLife.kt
@@ -1,9 +1,17 @@
 package gameoflife.main
 
 import gameoflife.game.Game
+import gameoflife.ui.Window
+
+const val INIT_GRID_SIZE = 20;
 
 fun main(args: Array<String>)
 {
-    val game = Game(Pair(20,20))
+    val window = Window()
+    val game = Game(Pair(INIT_GRID_SIZE, INIT_GRID_SIZE))
     game.run()
 }
+
+
+
+// This file should facilitate game operations
